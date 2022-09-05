4f40cee484710775b3436dd92f77c04f5cfc3452
lpeil <luan@peil.dev>
2022-09-05 18:01:18 -0300

    fix: adjust svgr to version 6.3.1

--- packages/ocean-icons-react/scripts/IconTemplate.ts
+++ packages/ocean-icons-react/scripts/IconTemplate.ts
@@ -1,23 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types */
-const IconTemplate = (
-  { template }: any,
-  _opts: any,
-  { componentName, jsx, exports }: any
-) => {
-  const tsTemplate = template.smart({ plugins: ['typescript'] });
-  return tsTemplate.ast`
-      import React from 'react';
-      ${'\n'}
-      type IconProps = {
-        size?: number;
-      } & React.SVGProps<SVGSVGElement>;
-      
-      const ${componentName} = ({ size = 24, ...props }: IconProps, svgRef: React.Ref<SVGSVGElement>) => (
-        ${jsx}
-      );
-      
-      ${exports}
-    `;
+const IconTemplate = ({ jsx, exports, componentName }: any, { tpl }: any) => {
+  return tpl`
+    import React, { forwardRef } from 'react';
+    ${'\n'}
+    type IconProps = {
+      size?: number;
+    } & React.SVGProps<SVGSVGElement>;
+    
+    const ${componentName} = ({ size = 24, ...props }: IconProps, ref: React.Ref<SVGSVGElement>) => (
+      ${jsx}
+    );
+    
+    ${exports}
+  `;
 };
 
 export default IconTemplate;
