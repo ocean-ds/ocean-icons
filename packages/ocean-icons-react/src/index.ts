9f22a0fe24b9f416cc44b1a77ee85c63f365478c
Douglas Aguiar Zanquetta de Oliveira <95656184+DazoBlu@users.noreply.github.com>
2023-01-18 13:22:07 -0300

    feat: add icon pagblu outline (#247)
    * feat: add icon pagblu outline
    
    * fix: run script for generate react component

--- packages/ocean-icons-react/src/index.ts
+++ packages/ocean-icons-react/src/index.ts
@@ -404,6 +404,7 @@ export { default as MusicNoteOutline } from './MusicNoteOutline';
 export { default as NewspaperOutline } from './NewspaperOutline';
 export { default as OfficeBuildingOutline } from './OfficeBuildingOutline';
 export { default as OrderOutline } from './OrderOutline';
+export { default as PagBluOutline } from './PagBluOutline';
 export { default as PaperAirplaneOutline } from './PaperAirplaneOutline';
 export { default as PaperClipOutline } from './PaperClipOutline';
 export { default as PauseOutline } from './PauseOutline';
