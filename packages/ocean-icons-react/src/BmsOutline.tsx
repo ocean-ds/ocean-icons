dded7ee90b44f0b3fca4d1db5500c8d66c8e0d09
DazoBlu <douglas.zanquetta@useblu.com.br>
2023-01-18 13:01:48 -0300

    fix: run script for generate react component

--- packages/ocean-icons-react/src/BmsOutline.tsx
+++ packages/ocean-icons-react/src/BmsOutline.tsx
@@ -12,24 +12,17 @@ const BmsOutline = (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 25"
+    stroke="currentColor"
     width={size}
     height={size}
     ref={ref}
     {...props}
   >
     <path
-      d="M20.618 6.48397C17.4561 6.65189 14.3567 5.55858 12 3.44397C9.64327 5.55858 6.5439 6.65189 3.382 6.48397C3.12754 7.46908 2.99918 8.48252 3 9.49997C3 15.091 6.824 19.79 12 21.122C17.176 19.79 21 15.092 21 9.49997C21 8.45797 20.867 7.44797 20.618 6.48397Z"
-      stroke="currentColor"
-      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-    />
-    <path
-      d="M10.5998 15V9M7.7998 15V9M13.3998 15V9M16.1998 15V9"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M20.618 6.484A11.955 11.955 0 0 1 12 3.444a11.955 11.955 0 0 1-8.618 3.04A12.02 12.02 0 0 0 3 9.5c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM10.6 15V9m-2.8 6V9m5.6 6V9m2.8 6V9"
     />
   </svg>
 );
