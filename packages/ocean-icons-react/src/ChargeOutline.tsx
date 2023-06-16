840378167ca653917a63e51eb08f8cab61030b8a
lpeil <luan@peil.dev>
2023-06-16 10:54:55 -0300

    feat: add report, charge and pagblu icons

--- packages/ocean-icons-react/src/ChargeOutline.tsx
+++ packages/ocean-icons-react/src/ChargeOutline.tsx
@@ -4,14 +4,14 @@ type IconProps = {
   size?: number;
 } & React.SVGProps<SVGSVGElement>;
 
-const BtokenOutlineOutline = (
+const ChargeOutline = (
   { size = 24, ...props }: IconProps,
   ref: React.Ref<SVGSVGElement>
 ) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    viewBox="0 0 20 20"
+    viewBox="0 0 24 24"
     stroke="currentColor"
     width={size}
     height={size}
@@ -22,10 +22,10 @@ const BtokenOutlineOutline = (
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
-      d="M17.182 4.987A9.963 9.963 0 0 1 10 2.453a9.962 9.962 0 0 1-7.182 2.534C2.606 5.807 2.5 6.652 2.5 7.5c0 4.66 3.187 8.575 7.5 9.685 4.313-1.11 7.5-5.025 7.5-9.685 0-.868-.11-1.71-.318-2.513z"
+      d="M14.5 9.5c-.25-.833-1.083-1.25-2.5-1.25m0 0c-2.167 0-2.5 1.25-2.5 1.917 0 2.583 5 1.25 5 3.75 0 .666-.333 1.833-2.5 1.833m0-7.5V7m0 8.75c-1.417 0-2.25-.417-2.5-1.25m2.5 1.25V17m9-5a9 9 0 1 1-18 0 9 9 0 0 1 18 0z"
     />
   </svg>
 );
 
-const ForwardRef = forwardRef(BtokenOutlineOutline);
+const ForwardRef = forwardRef(ChargeOutline);
 export default ForwardRef;
--- packages/ocean-icons-react/src/ReportOutline.tsx
+++ packages/ocean-icons-react/src/ReportOutline.tsx
@@ -4,14 +4,14 @@ type IconProps = {
   size?: number;
 } & React.SVGProps<SVGSVGElement>;
 
-const BtokenOutlineOutline = (
+const ReportOutline = (
   { size = 24, ...props }: IconProps,
   ref: React.Ref<SVGSVGElement>
 ) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    viewBox="0 0 20 20"
+    viewBox="0 0 24 24"
     stroke="currentColor"
     width={size}
     height={size}
@@ -20,12 +20,11 @@ const BtokenOutlineOutline = (
   >
     <path
       strokeLinecap="round"
-      strokeLinejoin="round"
       strokeWidth={2}
-      d="M17.182 4.987A9.963 9.963 0 0 1 10 2.453a9.962 9.962 0 0 1-7.182 2.534C2.606 5.807 2.5 6.652 2.5 7.5c0 4.66 3.187 8.575 7.5 9.685 4.313-1.11 7.5-5.025 7.5-9.685 0-.868-.11-1.71-.318-2.513z"
+      d="M8.5 12h7m-7 3.429H12M8.5 8.57h7M5 6.095C5 4.938 5.958 4 7.139 4h9.722C18.042 4 19 4.938 19 6.095v11.81C19 19.062 18.042 20 16.861 20H7.14C5.958 20 5 19.062 5 17.905V6.095z"
     />
   </svg>
 );
 
-const ForwardRef = forwardRef(BtokenOutlineOutline);
+const ForwardRef = forwardRef(ReportOutline);
 export default ForwardRef;
