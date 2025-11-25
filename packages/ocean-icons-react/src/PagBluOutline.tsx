3f9bcd1fa6c32554fe489c3ec40643742d82b63b
lpeil <luanpeil@gmail.com>
2025-11-25 10:02:16 -0300

    fix: adjust icons strokes

--- packages/ocean-icons-react/src/PagBluOutline.tsx
+++ packages/ocean-icons-react/src/PagBluOutline.tsx
@@ -18,9 +18,10 @@ const PagBluOutline = (
     ref={ref}
     {...props}
   >
-    <path strokeLinecap="round" d="M4 10h1m-3 4h3" />
+    <path strokeLinecap="round" strokeWidth={2} d="M4 10h1m-3 4h3" />
     <path
       strokeLinejoin="bevel"
+      strokeWidth={2}
       d="M10.571 4H8.857C8.384 4 8 4.358 8 4.8v14.4c0 .442.384.8.857.8h6.429C17.889 20 20 18.03 20 15.6c0-1.85-1.286-3.2-3.04-4.114 1.303-.686 2.183-1.99 2.183-3.486 0-2.21-1.919-4-4.286-4H9.714"
     />
     <path
